4d99a42985f93a69cfab808a0194b63baa0b0790
nataliewutc <nataliewu.261201@gmail.com>
2022-06-10 14:04:41 +0100

    pre-processing and training

--- main.py
+++ main.py
@@ -2,12 +2,10 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn import svm
-from typing import Union
-from scipy.stats import norm
 import seaborn as sns
-from typing import List
-import scipy.fft
 
+
+#Data cleaning 
 data = pd.read_csv('Crystal_structure.csv')
 data_replaced = data.copy()
 data_without_columns = data_replaced.drop(columns= ['In literature','v(A)','v(B)','τ'])
@@ -15,12 +13,32 @@ data_without_rows = data_without_columns.dropna()
 data_replace_dash = data_without_rows.replace('-',np.nan)
 data_replace_zero = data_replace_dash.replace(0, np.nan)
 data_replace = data_replace_zero.dropna()
-data_replace
-#add encoder
+
+#add one hot encoder
 data_onehotencoded = data_replace.copy()
 classes = set(data_onehotencoded['Lowest distortion'])
 for cls in classes:
     data_onehotencoded[cls] = data_onehotencoded['Lowest distortion'].apply(lambda x: 1 if x == cls else 0)
 del data_onehotencoded['Lowest distortion']
-data_onehotencoded
 data_onehotencoded.to_csv('Crystal_structure_preprocessed.csv')
+
+#Min-max scaling 
+data_minmax = data_onehotencoded.copy()
+name_columns = list(data_minmax.columns)
+for i in range(3, len(data_minmax.columns)-5):  
+    min_value = min(data_minmax[name_columns[i]])
+    max_value = max(data_minmax[name_columns[i]])
+    diff = int(max_value) - int(min_value)
+    data_minmax[name_columns[i]] = data_minmax[name_columns[i]].apply(lambda x: (x - min_value) / diff) 
+
+#Training and testing data split 
+y_col = name_columns.pop(13)
+X = data_minmax[name_columns].to_numpy()
+y = data_minmax[y_col].to_numpy()
+
+training_fraction = 0.1 #can be changed
+training_size =int(training_fraction*len(data_minmax))
+X_training = X[:training_size]
+X_test = X[training_size:]
+y_training = y[:training_size]
+y_test = y[training_size:]
