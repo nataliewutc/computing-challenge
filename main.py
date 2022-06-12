8910adea7eb5221f96449451a4cc443dd7d7ccf1
nataliewutc <nataliewu.261201@gmail.com>
2022-06-12 18:33:40 +0100

    Feature Importance

--- main.py
+++ main.py
@@ -203,6 +203,22 @@ tree_depth = get_tree_depth()
 tree_depth_data = get_scores(tree_depth, 'Tree depth')
 display(tree_depth_data)
 
+#Feature importance 
+start_time = time()
+result = permutation_importance(
+    forest, X_test, y_test, n_repeats=10, random_state=42, n_jobs=2
+)
+elapsed_time = time() - start_time
+print(f"Elapsed time to compute the importances: {elapsed_time:.3f} seconds")
+
+forest_importances = pd.Series(result.importances_mean, index=name_columns[:10])
+fig, ax = plt.subplots()
+forest_importances.plot.bar(ax=ax)
+ax.set_title("Feature importances using permutation on full model")
+ax.set_ylabel("Mean accuracy decrease")
+fig.tight_layout()
+plt.show()
+print(f"Most important feature: {forest_importances.idxmax(axis=1)}")
 
 
 
